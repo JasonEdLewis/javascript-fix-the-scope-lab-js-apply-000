238189bb1c054e89d3512503341b52a409509687
Jason Lewis <jasonedlewis@gmail.com>
2019-06-15 18:08:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,16 +12,15 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
 
   // Feel free to move things around!
-  const two = 2
+  const two = 2;
+   return n + two
 }
 
 var funkyFunction = function() {
-  return function() {
     return "FUNKY!"
-  }
+
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
